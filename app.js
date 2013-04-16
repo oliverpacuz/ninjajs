fb77b122df9871a150cdbc48d664d52ec052647a
Oliver Pacuz <oliverpacuz@outlook.com>
2013-04-16 15:39:37 +0800

    Add vcard and edited files

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-document.getElementById('first-name').innerHTML = "Modified Oliver";
-document.getElementById('last-name').innerHTML = "Modified Pacuz";
+//var divTags = document.getElementsByTagName('div');
+//console.log(document.getElementsByTagName('div')[1]);
+//console.log(document.getElementsByClassName('vcard'));
+
+
+var firstNameEl = document.getElementById('first-name');
+var lastNameEl = document.getElementById('last-name');
+
+//firstNameEl.addEventListener('click', function() { firstNameEl.innerHTML = 'Oliver Modified'; } );
+
+
+var info = {
+    firstName: "Jerome",
+    lastName: "Coloma"
+};
+
+
+firstNameEl.innerHTML = info.firstName;
+lastNameEl.innerHTML = info.lastName; 
